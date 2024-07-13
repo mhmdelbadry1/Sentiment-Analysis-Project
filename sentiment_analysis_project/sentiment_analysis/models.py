29fa9a5a60b1f2845fc3583c69c1c3625f93c949
mhmdelbadry1 <mohamed.hassan221012@gmail.com>
2024-07-13 10:00:43 +0300

    updated

--- sentiment_analysis_project/sentiment_analysis/models.py
+++ sentiment_analysis_project/sentiment_analysis/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 import os
 import pickle
-import numpy as np
 import re
-from nltk import PorterStemmer
 from nltk.corpus import stopwords
-
+import re
+import string
+import nltk
+# Download the WordNet resource
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 # Get the absolute path of the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,28 +30,73 @@ class SentimentAnalysisModel:
 
         with open(tfidf_path, 'rb') as tf:
             victorizer = pickle.load(tf)
-        print("Received text:", text)
+        print("Received text:", text.lower())
+       
         # Transform the input text using TF-IDF vectorizer
-        comment_cleaned = datacleaning(text)
+        comment_cleaned = preprocess_text(text)
+        print(comment_cleaned)
         TextVictorized = victorizer.transform([comment_cleaned])
 
-        
+        print(TextVictorized)
+     
          
-
+         
      
         sentiment = model.predict(TextVictorized)
-
         return sentiment
-    
-stopwords_set = set(stopwords.words('english'))
-emoji_pattern = re.compile(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251\U0001F910-\U0001F9FF]+', flags=re.UNICODE)
-def datacleaning(text):
-    text = re.sub('<[^>]*>', ' ', text)  # Removing HTML tags
-    text = emoji_pattern.sub('', text)  # Removing emojis
-    text = re.sub(r'\W', ' ', text.lower())  # Removing non-word characters and converting to lowercase
-    prter = PorterStemmer()
-    text = [prter.stem(word) for word in text.split() if word not in stopwords_set]  # Stemming and removing stopwords
-    return " ".join(text)
 
 
-    
+def remove_emojis(text):
+
+    """Remove emojis from the text."""
+    if isinstance(text, str):
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002500-\U00002BEF"  # chinese char
+                                   u"\U00002702-\U000027B0"
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u"\U00010000-\U0010ffff"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u200d"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\ufe0f"  # dingbats
+                                   u"\u3030"
+                                   "]+", flags=re.UNICODE)
+        return emoji_pattern.sub(r'', text)
+    else:
+        return text
+def preprocess_text(text):
+        # Normalization
+        text = text.lower()
+        
+        # Remove HTML tags
+        text = re.sub(r'<.*?>', '', text)
+        
+        # Remove URLs
+        text = re.sub(r'http\S+|www\S+', '', text)
+        
+        # Remove numbers
+        text = re.sub(r'\d+', '', text)
+        
+        # Remove punctuation
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        
+        # Tokenize text
+        tokens = word_tokenize(text)
+        
+        # Remove stopwords
+        stop_words = set(stopwords.words('english'))
+        tokens = [word for word in tokens if word not in stop_words]
+        
+        # Remove emojis
+        tokens = [remove_emojis(word) for word in tokens]
+        print(tokens)
+        return ' '.join(tokens)
--- sentiment_analysis_project/sentiment_analysis/tests.py
+++ sentiment_analysis_project/sentiment_analysis/tests.py
@@ -7,36 +7,36 @@ class SentimentAnalysisModelTestCase(TestCase):
 
     def test_negative_sentiment(self):
         text = "I hate this product."  
-        expected_sentiment = 0  
+        expected_sentiment = "Negative"  
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
     def test_positive_sentiment(self):
         text = "I love this product!" 
-        expected_sentiment = 2  
+        expected_sentiment = "Positive" 
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
     def test_neutral_sentiment(self):
         text = "This product is okay." 
-        expected_sentiment = 1  
+        expected_sentiment = "Neutral" 
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
     def test_very_positive_sentiment(self):
         text = "This product is amazing!" 
-        expected_sentiment = 2  
+        expected_sentiment = "Positive" 
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
     def test_mixed_sentiment(self):
         text = "I like some features but dislike others." 
-        expected_sentiment = 1  
+        expected_sentiment = "Neutral"  
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
     def test_special_characters(self):
         text = "This product is $%#@^&" 
-        expected_sentiment = 2
+        expected_sentiment = "irrelevant"
         predicted_sentiment = self.sentiment_model.analyze(text)
         self.assertEqual(predicted_sentiment, expected_sentiment)
